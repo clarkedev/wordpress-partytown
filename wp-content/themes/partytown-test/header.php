22f9686104d4edf9e9230bcd7a17076ca216aa12
Will Clarke <willc@popcreative.co.uk>
2023-10-27 09:25:39 +0100

    init

---
+++ wp-content/themes/partytown-test/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <script src="~partytown/partytown.js"></script>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php wp_head(); ?>
+
+
+    <script>
+        partytown = {
+            forward: ['dataLayer.push'],
+        };
+    </script>
+
+    <script type="text/partytown">
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-KW37VNM');
+    </script>
+</head>
+<body>
